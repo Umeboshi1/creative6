7dd095465eef1ad77b36d82ff378a16ccc98d7f1
Umeboshi1 <31867288+Umeboshi1@users.noreply.github.com>
2017-12-13 08:38:20 -0700

    Update users_model.js

--- models/users_model.js
+++ models/users_model.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 var UserSchema = new Schema({
     username: { type: String, unique: true },
-    email: String,
+    pic: String,
     color: String,
     hashed_password: String
 });
